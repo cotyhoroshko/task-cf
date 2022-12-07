de69d9d7dd4d0f85cd7433b5c3853eaf48def0c7
Andrii Mruts <cotyhoroshko@gmail.com>
2022-12-07 11:04:53 +0200

    Add dependencies, format terraform

--- main.tf
+++ main.tf
@@ -11,52 +11,52 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "task-cf-bucket" {
-    name     = "${var.project_id}-bucket"
-    location = var.region
-    force_destroy = true
+  name          = "${var.project_id}-bucket"
+  location      = var.region
+  force_destroy = true
 }
 
 data "archive_file" "source" {
-    type        = "zip"
-    source_dir  = "./function"
-    output_path = "/tmp/function.zip"
+  type        = "zip"
+  source_dir  = "./function"
+  output_path = "/tmp/function.zip"
 }
 
 resource "google_storage_bucket_object" "zip" {
-    source       = data.archive_file.source.output_path
-    content_type = "application/zip"
+  source       = data.archive_file.source.output_path
+  content_type = "application/zip"
 
-    name         = "src-${data.archive_file.source.output_md5}.zip"
-    bucket       = google_storage_bucket.task-cf-bucket.name
+  name   = "src-${data.archive_file.source.output_md5}.zip"
+  bucket = google_storage_bucket.task-cf-bucket.name
 
-    depends_on   = [
-        google_storage_bucket.task-cf-bucket,
-        data.archive_file.source
-    ]
+  depends_on = [
+    google_storage_bucket.task-cf-bucket,
+    data.archive_file.source
+  ]
 }
 
 resource "google_cloudfunctions_function" "task-cf-function" {
-    name                  = "task-cf-function"
-    runtime               = "python39"
+  name    = "task-cf-function"
+  runtime = "python39"
 
-    source_archive_bucket = google_storage_bucket.task-cf-bucket.name
-    source_archive_object = google_storage_bucket_object.zip.name
+  source_archive_bucket = google_storage_bucket.task-cf-bucket.name
+  source_archive_object = google_storage_bucket_object.zip.name
 
-    entry_point           = "main"
-    trigger_http          = true
+  entry_point  = "main"
+  trigger_http = true
 
-    environment_variables = {
-      FUNCTION_REGION = var.region
-      GCP_PROJECT = var.project_id
-      DATASET_ID = var.dataset_id
-      OUTPUT_TABLE = google_bigquery_table.task-cf-table.table_id
-    }
+  environment_variables = {
+    FUNCTION_REGION = var.region
+    GCP_PROJECT     = var.project_id
+    DATASET_ID      = var.dataset_id
+    OUTPUT_TABLE    = google_bigquery_table.task-cf-table.table_id
+  }
 
-    depends_on            = [
-        google_storage_bucket.task-cf-bucket,
-        google_storage_bucket_object.zip,
-        google_bigquery_dataset.task-cf-dataset
-    ]
+  depends_on = [
+    google_storage_bucket.task-cf-bucket,
+    google_storage_bucket_object.zip,
+    google_bigquery_dataset.task-cf-dataset
+  ]
 }
 
 resource "google_cloudfunctions_function_iam_member" "invoker" {
@@ -66,27 +66,31 @@ resource "google_cloudfunctions_function_iam_member" "invoker" {
 
   role   = "roles/cloudfunctions.invoker"
   member = "allUsers"
+
+  depends_on = [
+    google_cloudfunctions_function.task-cf-function
+  ]
 }
 
 resource "google_bigquery_dataset" "task-cf-dataset" {
-  dataset_id = var.dataset_id
-  description                 = "This dataset is public"
-  location                    = "US"
+  dataset_id  = var.dataset_id
+  description = "This dataset is public"
+  location    = "US"
 }
 
 resource "google_bigquery_table" "task-cf-table" {
   dataset_id = var.dataset_id
   table_id   = var.table_id
-  schema = file("schemas/bq_table_schema/task-cf-raw.json")
+  schema     = file("schemas/bq_table_schema/task-cf-raw.json")
 
   depends_on = [
-      google_bigquery_dataset.task-cf-dataset
+    google_bigquery_dataset.task-cf-dataset
   ]
 }
 
 resource "google_cloudbuild_trigger" "github-trigger" {
-  project = var.project_id
-  name = "github-updates-trigger"
+  project  = var.project_id
+  name     = "github-updates-trigger"
   filename = "cloudbuild.yaml"
   github {
     owner = "cotyhoroshko"
