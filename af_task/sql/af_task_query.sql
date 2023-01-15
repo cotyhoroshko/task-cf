d94005213d5f6029c55ebdc693bfa52075d1d1cd
Andrii Mruts <anmrut@softserveinc.com>
2023-01-16 01:12:17 +0200

    Update sql

--- af_task/sql/af_task_query.sql
+++ af_task/sql/af_task_query.sql
@@ -2,7 +2,8 @@ WITH
 airflow_data AS (
     SELECT
         name,
-        CHAR_LENGTH(name) as length
+        CHAR_LENGTH(name) as "length",
+        timestamp
     FROM
         `{{ params.AF_TASK_INPUT_TABLE }}`
 )
