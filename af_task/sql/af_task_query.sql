04f2b40385c5416751c83bdcb91a3b42720c901d
Andrii Mruts <anmrut@softserveinc.com>
2023-01-16 01:15:33 +0200

    Add current timestamp to af sql

--- af_task/sql/af_task_query.sql
+++ af_task/sql/af_task_query.sql
@@ -2,8 +2,8 @@ WITH
 airflow_data AS (
     SELECT
         name,
-        CHAR_LENGTH(name) as "length",
-        timestamp
+        CHAR_LENGTH(name) as length
+        CURRENT_TIMESTAMP() as timestamp
     FROM
         `{{ params.AF_TASK_INPUT_TABLE }}`
 )
--- af_task/task_af.py
+++ af_task/task_af.py
@@ -10,7 +10,7 @@ DEFAULT_DAG_ARGS = {
 }
 
 with DAG(
-        "af_task_job",
+        "airflow_task_job",
         schedule_interval="0 * * * *",
         default_args={
             'start_date': datetime(2023, 1, 15),
