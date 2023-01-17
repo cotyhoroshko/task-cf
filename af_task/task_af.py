2f5d5eec6c6391cad9f1c5aadab24849f99fd40e
Andrii Mruts <anmrut@softserveinc.com>
2023-01-17 10:35:05 +0200

    Demo

--- af_task/task_af.py
+++ af_task/task_af.py
@@ -11,7 +11,7 @@ DEFAULT_DAG_ARGS = {
 
 with DAG(
         "airflow_last_hour_job",
-        schedule_interval="28 * * * *",
+        schedule_interval="37 * * * *",
         default_args={
             'start_date': datetime(2023, 1, 17),
             **DEFAULT_DAG_ARGS,
